34cc2dd9487c1a62eb15093cd733e6df24a26471
walkccc <walkccray@gmail.com>
2021-01-16 02:12:30 -0500

    [server] Support cookie login

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 import { ApolloServer } from 'apollo-server-express';
+import cookieParser from 'cookie-parser';
 import express, { Application } from 'express';
 
 import { connectDatabase } from './database';
@@ -9,10 +10,13 @@ import { typeDefs, resolvers } from './graphql';
 
 const mount = async (app: Application): Promise<void> => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db }),
+    context: ({ req, res }) => ({ db, req, res }),
   });
   server.applyMiddleware({ app, path: '/api' });
 
